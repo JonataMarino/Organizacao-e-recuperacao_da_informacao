879f06fc5df9f694e6fc71288f676879a84a8f55
Jonatã Serdan Marino <jsmarino1993@outlook.com>
2022-09-29 20:51:37 -0300

    Ex_29_09_22

--- Exercicios/Atividade Aula pratica/teste/SqlScript/tb_cidadao.sql
+++ Exercicios/Atividade Aula pratica/teste/SqlScript/tb_cidadao.sql
@@ -1,22 +1,19 @@
--- selecione todas as colunas da tabela tb_aula
 SELECT * FROM db_acervom.tb_acervom;
 
--- selecione a coluna ID e NAME  `NAME` <- para palavras reservadas use a crase
-select ID, NOME from  db_acervom.tb_acervom;
-
 -- para inserir uma informação na tabela
-INSERT INTO db_acervom.tb_acervom (NOME, PAIS) VALUES ('Jonatã', 'Brasil');
+INSERT INTO db_acervom.tb_acervom (NOME, PAIS, ESTADO, CIDADE, BAIRRO, LOGRADOURO, NUMERO, COMPLEMENTO ) VALUES ('', '', '', '', '', '', '', '');
 
 -- para atualizar informação na tabela
 UPDATE db_acervom.tb_acervom
 	SET NOME = "",
 		PAIS = "",
-        ESTADO = "",
         CIDADE = "",
+        ESTADO = "",
         BAIRRO = "",
         LOGRADOURO = "",
         NUMERO = "",
         COMPLEMENTO = ""
-	WHERE ID = 1;
+        
+	WHERE ID = ID;
     
-delete from db_acervom.tb_acervom where ID = NULL;
+delete from db_acervom.tb_acervom where idtb_acervom = 1;
